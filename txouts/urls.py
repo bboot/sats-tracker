d16f3b55f25fd9201e08a8e471f91802b5f6fef6
Bryan Boot <bboot1@gmail.com>
2021-11-28 15:53:45 -0800

    make ui useable

--- txouts/urls.py
+++ txouts/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
 
-from .views import TxOutDetailView, TxOutListView
+from .views import TxOutDetailView, TxOutListView, ActorListView
 
 urlpatterns = [
     path('<int:pk>/', TxOutDetailView.as_view(), name="txout_detail"),
+    path('actors/', ActorListView.as_view(), name="actor_list"),
     path('', TxOutListView.as_view(), name="txout_list"),
 ]
--- txouts/views.py
+++ txouts/views.py
@@ -1,6 +1,6 @@
 from django.views.generic import DetailView, ListView
 
-from .models import TxOut
+from .models import TxOut, Actor
 
 # Create your views here.
 class TxOutDetailView(DetailView):
@@ -11,3 +11,13 @@ class TxOutDetailView(DetailView):
 class TxOutListView(ListView):
     model = TxOut
     template_name = "txout_list.html"
+
+
+class ActorDetailView(DetailView):
+    model = Actor
+    template_name = "actor_detail.html"
+
+
+class ActorListView(ListView):
+    model = Actor
+    template_name = "actor_list.html"
