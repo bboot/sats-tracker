d71a3625ed9bbaf35626f542db5c1af09b00f866
Bryan Boot <bboot1@gmail.com>
2022-01-03 22:52:11 -0800

    changed mind on using user password to encrypt db
    - leave the code there for edification

--- user_profile/views.py
+++ user_profile/views.py
@@ -2,7 +2,15 @@ from django.contrib.auth.views import (
         LoginView,
     )
 from django.shortcuts import render
-from django.contrib.auth.hashers import get_hasher
+from django.contrib.auth.hashers import get_hasher, Argon2PasswordHasher
+
+
+class MyArgon2PasswordHasher(Argon2PasswordHasher):
+    def verify(self, password, encoded):
+        argon2 = self._load_library()
+        hasher = argon2.PasswordHasher()
+        return super().verify(password, encoded)
+
 
 
 class UserLoginView(LoginView):
@@ -11,11 +19,21 @@ class UserLoginView(LoginView):
         return super().post(request, *args, **kwargs)
 
     def get_success_url(self):
-        """Return the URL to redirect to after processing a valid form."""
+        '''
+        Going to use the raw password + a salt to generate the
+        key for decrypting the database entries.
+        NB: If this were a multi-user app, then use js and end-to-end
+        encryption, as that is the only legitimate (read: trustless)
+        way to protect the user's private data.
+
+        TAKE 2: Not a good idea, just use the key stored on the
+        server. If you want to use the user's key for encryption,
+        then do end-to-end.
+        '''
         user = self.request.user
         algo, *items = user.password.split('$')
         hasher = get_hasher(algo)
-        print('@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ password items', items)
+        assert algo == 'argon2', 'Was expecting Argon2 password hasher.'
         return super().get_success_url()
 
 
