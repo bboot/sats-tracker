614a61df8fe72be60bfd5e9e8ce124ee5cc433e1
Bryan Boot <bboot1@gmail.com>
2022-04-26 17:21:02 -0700

    tweaks

--- generate-env.py
+++ generate-env.py
@@ -15,9 +15,9 @@ export DB_PASSWORD={data['user_password']}
 export DATABASE_URL=postgres://sats_trackeruser:$DB_PASSWORD@{args.hostname}:5432/sats_tracker
 # Bitcoind
 export RPC_PORT=8332
-export RPC_USER=<RPC user from your bitcoin core installation>
-export RPC_PASSWORD=<RPC password from your bitcoin core installation>
-export BITCOIND_HOST=<IP address of host that is running bitcoin core>
+export RPC_USER='<RPC user from your bitcoin core installation>'
+export RPC_PASSWORD='<RPC password from your bitcoin core installation>'
+export BITCOIND_HOST='<IP address of host that is running bitcoin core>'
 export BITCOIN_NETWORK=mainnet
 # Electrum
 export ELECTRUM_SERVICES=rpc://{args.electrum_host}:8000,tcp://{args.electrum_host}:50001
@@ -25,6 +25,18 @@ export ELECTRUM_SERVICES=rpc://{args.electrum_host}:8000,tcp://{args.electrum_ho
 export EXPLORER=http://umbrel.local:3002
 """
     return content
+
+def get_pg_setup_txt(args, data):
+    postgres_setup_txt=f'''
+sudo -u postgres psql -c "
+CREATE USER sats_trackeruser WITH PASSWORD \'{data['user_password']}\';
+ALTER ROLE sats_trackeruser SET client_encoding TO 'utf8';
+ALTER ROLE sats_trackeruser SET default_transaction_isolation TO 'read committed';
+ALTER ROLE sats_trackeruser SET timezone TO 'UTC';
+GRANT ALL PRIVILEGES ON DATABASE sats_tracker TO sats_trackeruser;
+"
+'''
+    return postgres_setup_txt
 
 def gen_encryption_material(args):
     data = {}
@@ -41,10 +53,16 @@ def main(args):
     cwd = Path(__file__).parent.resolve()
     fenv = Path(cwd.joinpath('.env'))
     if fenv.is_file():
-        print(f'Not overwriting existing file at {fenv}')
+        print(f'Not overwriting existing .env file at {fenv}')
         return 1
     with open(fenv, 'w') as f:
         f.write(txt)
+    pgsetup = Path(cwd.joinpath('pgsetup.sh'))
+    if pgsetup.is_file():
+        print(f'Not overwriting existing pgsetup.sh file at {pgsetup}')
+        return 1
+    with open(pgsetup, 'w') as f:
+        f.write(get_pg_setup_txt(args, data))
     return 0
 
 
